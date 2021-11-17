690e68e77928caa3f1a538376eeb01c10d6a0d9e
parkwilliamy <94515539+parkwilliamy@users.noreply.github.com>
2021-11-17 11:30:07 -0800

    Update LifeGame.py

--- LifeGame.py
+++ LifeGame.py
@@ -6,64 +6,65 @@ class Cell(Fl_Button):
 		Fl_Button.__init__(self,x,y,w,h,label)
 		
 	
-class Grid(Fl_Window):
+class Grid(Fl_Double_Window):
 	def butcb(self, wid):
 		wid.color(FL_YELLOW)
 		
 	def startcb(self, wid):
+		Cell.alive=0
 		contact=0
-		boo=True
-		while boo==True:
-			for z in range(len(self.bl)):
-				if self.bl[z].color()==95:
-					Cell.alive+=1
-					for a in self.touch:
-						if z+a < 0 or z+a > 6400:
-							continue
-						if self.bl[z+a].color()==FL_YELLOW:
-							contact+=1
-							
-					if contact < 2:
-						self.bl[z].color(FL_BACKGROUND_COLOR)
-						self.bl[z].redraw()
-						Cell.alive=Cell.alive-1
-					elif contact >= 4: 
-						self.bl[z].color(FL_BACKGROUND_COLOR)
-						self.bl[z].redraw()
-						Cell.alive=Cell.alive-1
+		born=[]
+		kill=[]
+		for z in range(len(self.bl)):
+			if self.bl[z].color()==95:
+				contact=0
+				Cell.alive+=1
+				for a in self.touch:
+					if self.bl[z+a].color()==FL_YELLOW:
+						contact+=1
 						
-				else:
-					for a in self.touch:
-						if z+a < 0 or z+a > 6400 or z+a == 6321:
-							continue
-						if self.bl[z+a].color()==FL_YELLOW:
-							contact+=1
-							
-					if contact >= 3:
-						self.bl[z].color(FL_YELLOW)
-						self.bl[z].redraw()
-						Cell.alive+=1
-			
-			print(Cell.alive)
-			if Cell.alive==0:
-				boo=False
-			
+				if contact < 2 or contact >= 4:
+					kill.append(self.bl[z]) #adds the cell to be killed
+				
+				
+			else:
+				for a in self.touch:
+					if z+a < 0 or z+a > 6399:
+						continue
+					
+					if self.bl[z+a].color()==FL_YELLOW:
+						contact+=1
+					
+				if contact >= 3:
+					born.append(self.bl[z])
+				
+		for cell in born:
+			cell.color(FL_YELLOW)
+			cell.redraw()
+						
 		
-						
+		'''		
+		for cell in kill:
+			cell.color(FL_BACKGROUND_COLOR)
+			cell.redraw()
+			'''
+		
+		
 					
+			
+				
+			
+				
 				
 					
-					
-				
-				
 		
 				
 		
 				
 				
 	def __init__(self,x,y,w,h,label=None):
-		Fl_Window.__init__(self, x, y, w, h, label)
-		self.touch=[-80,80,-1,1,-81,-79,79,81]
+		Fl_Double_Window.__init__(self, x, y, w, h, label)
+		self.touch=[-1,79,-81,-80,80,1,-79, 81] #first 3 are left, last 3 are right
 		self.width=10
 		self.bl=[]
 		self.begin()
